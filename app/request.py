ff05a91c642aaabddd60c47aeb5bef0bd5a639e3
LekamCharity.gmail.com <LekamCharity.gmail.com>
2020-10-13 19:18:07 +0300

    added a navbar

--- app/request.py
+++ app/request.py
@@ -10,11 +10,12 @@ api_key = app.config['MOVIE_API_KEY']
 # Getting the movie base url
 base_url = app.config["MOVIE_API_BASE_URL"]
 
+
 def get_movies(category):
     '''
     Function that gets the json response to our url request
     '''
-    get_movies_url = base_url.format(category,api_key)
+    get_movies_url = base_url.format(category, api_key)
 
     with urllib.request.urlopen(get_movies_url) as url:
         get_movies_data = url.read()
@@ -26,8 +27,8 @@ def get_movies(category):
             movie_results_list = get_movies_response['results']
             movie_results = process_results(movie_results_list)
 
-
     return movie_results
+
 
 def process_results(movie_list):
     '''
@@ -49,7 +50,10 @@ def process_results(movie_list):
         vote_count = movie_item.get('vote_count')
 
         if poster:
-            movie_object = Movie(id,title,overview,poster,vote_average,vote_count)
+            movie_object = Movie(id, title, overview,
+                                 poster, vote_average, vote_count)
             movie_results.append(movie_object)
 
     return movie_results
+
+ 
